c36ee190e4a926ace085c62acf85540e4ad320fb
sarthakz-47 <sarthakz.4100@gmail.com>
2026-06-30 03:49:23 +0530

    builded signup/login page

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+import Login from "./pages/login"
+import { LogIn } from "lucide-react"
+
+function App() {
+  return (
+    <main>
+      <Login />
+    </main>
+  )
+}
+
+export default App
