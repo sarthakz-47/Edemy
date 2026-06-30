dba8519f3166346527ac3c7d2da56eca172e0849
sarthakz-47 <sarthakz.4100@gmail.com>
2026-07-01 04:57:54 +0530

    User Model & Controllers

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import connectDB from "./database/db.js";
+import userRoute from "./routes/user.route.js"
+dotenv.config({})
+
+// call database connection here
+connectDB();
+const app = express();
+
+// default middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:8080",
+    credentials: true
+}))
+
+// apis
+app.use("/api/v1/user", userRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+}
+)
