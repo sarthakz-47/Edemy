71f233dc9172a6400ccbfc5cd2e08b9cec6b11b2
sarthakz-47 <sarthakz.4100@gmail.com>
2026-07-03 21:10:28 +0530

    Hero Setion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import { Button } from "@/components/ui/button";
 import Login from "./pages/login";
 import { LogIn } from "lucide-react";
 import Navbar from "./components/Navbar";
+import HeroSection from "./pages/student/HeroSection.jsx";
 function App() {
   return (
     <main>
       <Navbar />
+      <HeroSection />
       <Login />
     </main>
   );
