f54e0cdce7e8acc1080c9a2a495bec953be9a03f
sarthakz-47 <sarthakz.4100@gmail.com>
2026-07-04 01:06:01 +0530

    Build Courses Page

--- client/src/pages/student/Courses.jsx
+++ client/src/pages/student/Courses.jsx
@@ -1,20 +1,20 @@
 import { Skeleton } from "@/components/ui/skeleton";
 import React from "react";
 import Course from "./Course";
+
+const courses = [1, 2, 3, 4, 5, 6];
 const Courses = () => {
-  const isLoading = true;
+  const isLoading = false;
   return (
     <div className="bg-gray-50">
       <div className="max-w-7xl mx-auto p-6">
         <h2 className="font-bold text-3xl text-center mb-10">Our Courses</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {isLoading ? (
-            Array.from({ length: 8 }).map((_, index) => (
-              <CourseSkeleton key={index} />
-            ))
-          ) : (
-            <Course />
-          )}
+          {isLoading
+            ? Array.from({ length: 8 }).map((_, index) => (
+                <CourseSkeleton key={index} />
+              ))
+            : courses.map((courses, index) => <Course key={index} />)}
         </div>
       </div>
     </div>
